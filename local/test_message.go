a7ce072bdb1c84fbb5b23b75896eaa8b5a22e75a
Zach Kelling <z@zeekay.io>
2023-12-13 22:07:14 +0100

    Update gitignore

---
+++ local/test_message.go
@@ -0,0 +1,35 @@
+package local
+
+import "github.com/luxdefi/node/message"
+
+var _ message.OutboundMessage = &TestMsg{}
+
+type TestMsg struct {
+	op               message.Op
+	bytes            []byte
+	bypassThrottling bool
+}
+
+func NewTestMsg(op message.Op, bytes []byte, bypassThrottling bool) *TestMsg {
+	return &TestMsg{
+		op:               op,
+		bytes:            bytes,
+		bypassThrottling: bypassThrottling,
+	}
+}
+
+func (m *TestMsg) BypassThrottling() bool {
+	return m.bypassThrottling
+}
+
+func (m *TestMsg) Op() message.Op {
+	return m.op
+}
+
+func (m *TestMsg) Bytes() []byte {
+	return m.bytes
+}
+
+func (*TestMsg) BytesSavedCompression() int {
+	return 0
+}
